fd1b6a9b5c19ad742a49e6fa1f4ba214a989bd56
Delta Blendea <robertblendea@outlook.com>
2024-06-25 08:41:35 -0700

    Implement saving and loading

--- __main__.py
+++ __main__.py
@@ -1,7 +1,22 @@
 from pyboy import PyBoy
+import keyboard
+
+def on_key_event(event):
+    if keyboard.is_pressed('v'):
+        if event.name.isdigit():
+            with open("state_file_" + event.name + ".state", "wb") as f:
+                pyboy.save_state(f)
+    if keyboard.is_pressed('l'):
+        if event.name.isdigit():
+            with open("state_file_" + event.name + ".state", "rb") as f:
+                pyboy.load_state(f)
+
+keyboard.hook(on_key_event)
 
 pyboy = PyBoy('Pokemon - Red Version (USA, Europe).gb', sound=True)
 pyboy.set_emulation_speed(1)
+
 while pyboy.tick():
     pass
+
 pyboy.stop()
