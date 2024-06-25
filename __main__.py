b6cea491169df7cd8b5a98833a450faab3d511ae
Delta Blendea <robertblendea@outlook.com>
2024-06-25 13:08:37 -0700

    Clean up code

--- __main__.py
+++ __main__.py
@@ -4,16 +4,19 @@ import mem_scanner
 
 def on_key_event(event):
 
+    # Press v + number to save to that save slot
     if keyboard.is_pressed('v'):
         if event.name.isdigit():
             with open("state_file_" + event.name + ".state", "wb") as f:
                 pyboy.save_state(f)
 
+    # Press l + number to load that save
     if keyboard.is_pressed('l'):
         if event.name.isdigit():
             with open("state_file_" + event.name + ".state", "rb") as f:
                 pyboy.load_state(f)
 
+    # Press m to activate the memory scanner. 
     if keyboard.is_pressed('m'):
         scan.update_mem(pyboy.memory[0x0000:0xFFFF])
         pyboy._pause()
@@ -25,9 +28,7 @@ def on_key_event(event):
         print(hex_matches)
         pyboy._unpause()
 
-
 keyboard.hook(on_key_event)
-
 
 pyboy = PyBoy('Pokemon - Red Version (USA, Europe).gb', sound=True)
 pyboy.set_emulation_speed(1)
