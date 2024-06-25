72cb15ba27e6a0c9d750ed8b919113d954885fc6
Delta Blendea <robertblendea@outlook.com>
2024-06-25 13:08:51 -0700

    Add probable memory values to mem_values.py

---
+++ mem_values.py
@@ -0,0 +1,9 @@
+
+pkmn1_hp = 0xD16D
+pkmn2_hp = 0xD199
+pkmn3_hp = 0xD1C5
+
+# Guess based on gap between previous vals
+pkmn4_hp = 0xD1F1 # 0xCC54 and 0xCF9A also seem to match
+pkmn5_hp = 0xD21D
+pkmn6_hp = 0xD249
