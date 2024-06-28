a0865ba08d2bd5ce3db1b26e0d020b48d0b860d3
Evorp <3vorpgaming@gmail.com>
2024-06-28 12:06:29 -0700

    Add Python type hints

--- __main__.py
+++ __main__.py
@@ -1,39 +1,40 @@
 from pyboy import PyBoy
+from MemScanner import MemScanner
 import keyboard
-import mem_scanner
 
-def on_key_event(event):
 
+def on_key_event(event: keyboard.KeyboardEvent):
     # Press v + number to save to that save slot
     if keyboard.is_pressed('v'):
         if event.name.isdigit():
-            with open("state_file_" + event.name + ".state", "wb") as f:
+            with open(f"state_file_{event.name}.state", "wb") as f:
                 pyboy.save_state(f)
 
     # Press l + number to load that save
     if keyboard.is_pressed('l'):
         if event.name.isdigit():
-            with open("state_file_" + event.name + ".state", "rb") as f:
+            with open(f"state_file_{event.name}.state", "rb") as f:
                 pyboy.load_state(f)
 
-    # Press m to activate the memory scanner. 
+    # Press m to activate the memory scanner.
     if keyboard.is_pressed('m'):
         scan.update_mem(pyboy.memory[0x0000:0xFFFF])
         pyboy._pause()
         val = input()
         scan.set_val(int(val))
         scan.scan()
-        print (scan.matches)
+        print(scan.matches)
         hex_matches = [hex(x) for x in scan.matches_index]
         print(hex_matches)
         pyboy._unpause()
+
 
 keyboard.hook(on_key_event)
 
 pyboy = PyBoy('Pokemon - Red Version (USA, Europe).gb', sound=True)
 pyboy.set_emulation_speed(1)
 
-scan = mem_scanner.mem_scanner(pyboy.memory[0x0000:0xFFFF])
+scan = MemScanner(pyboy.memory[0x0000:0xFFFF])
 scan.init_scan()
 
 while pyboy.tick():
