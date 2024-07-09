eff051d73c3d9d5dab08648ea9192f7a4722d070
Delta Blendea <robertblendea@outlook.com>
2024-07-09 15:24:14 -0700

    Data gathering

--- __main__.py
+++ __main__.py
@@ -4,6 +4,7 @@ from MemScanner import MemScanner
 import mem_values
 from Player import Player
 import keyboard
+import csv
 
 
 def on_key_event(event: keyboard.KeyboardEvent):
@@ -31,6 +32,9 @@ def on_key_event(event: keyboard.KeyboardEvent):
         print(hex_matches)
         pyboy._unpause()
 
+    if keyboard.is_pressed('/'):
+        pyboy.stop()
+
 
 keyboard.hook(on_key_event)
 
@@ -47,70 +51,153 @@ counter = -1
 wins = 0
 losses = 0
 fightnum = 0
+crit_flag = 0
+miss_flag = 0
+
 
 rand = random.SystemRandom()
 
-battleData = []
+header = [
+    'BattleNum',
+    'Pokemon',
+    'Move',
+    'Crit?',
+    'Miss?',
+    'PlayerHP',
+    'OpponentHP'
+]
 
-while pyboy.tick():
-    print (battleData)
-    if counter == -1:
-        battleData.append([])
-        pyboy.tick(20)
-        if pyboy.memory[0xCC26] == 0 and pyboy.memory[0xCC25] == 9:
-            pyboy.button('a')
-            pyboy.tick(10)
-            pyboy.button ('down')
-            pyboy.tick(10)
-            pyboy.button ('down')
-            pyboy.tick(10)
-            pyboy.button ('a')
-            battleData[fightnum].append(pyboy.memory[0xCCDC])
+header2 = [
+    'Win',
+    'Turns'
+]
+
+movedict = {
+    73: 'leech seed',
+    45: 'growl',
+    33: 'tackle',
+    111: 'defense curl',
+    117: 'bide',
+    103: 'screech'
+}
+
+pokedex = {
+    34: 'onix',
+    169: 'geodude'
+}
+
+turns = 0
+
+with open ('BattleData.csv', 'w', encoding='UTF8', newline = '') as f:
+    writer = csv.writer (f)
+    writer.writerow(header2)
+
+    while pyboy.tick():
+        if pyboy.memory[0xD05E] == 1:
+            crit_flag = 1
+        if pyboy.memory[0xD05D] == 1:
+            miss_flag = 1
+        
+        if counter == -1:
             
-    
-    counter += 1
-    if counter % 5 == 0:
-        if pyboy.memory[0xCC26] == 0 and pyboy.memory[0xCC25] == 9:
-            if (pyboy.memory[0xCCDD] != 0):
-                battleData[fightnum].append(pyboy.memory[0xCCDD])
-            pyboy.button ('a')
-            pyboy.tick(10)
-            if (pyboy.memory[0xD02F] == 0):
-                attack = rand.randint(1, 2)
-            else :
-                attack = rand.randint(1, 3)
-            while pyboy.memory[0xCC26] != attack:
+            pyboy.tick(20)
+            if pyboy.memory[0xCC26] == 0 and pyboy.memory[0xCC25] == 9:
+                turns += 1
+                pyboy.button('a')
+                pyboy.tick(10)
                 pyboy.button ('down')
                 pyboy.tick(10)
-            pyboy.button ('a')
-            pyboy.tick(10)
-            battleData[fightnum].append(pyboy.memory[0xCCDC])
-        elif (pyboy.memory[0xCC26] >=0 and pyboy.memory[0xCC26] <= 2) and pyboy.memory[0xCC25] == 5:
-            pyboy.button ('a')
-            pyboy.tick(10)
-        else:
-            if pyboy.memory[0xCC26] == 1:
-                pyboy.button ('up')
-            if pyboy.memory[0xCC25] == 15:
-                pyboy.button ('left')
+                pyboy.button ('down')
+                pyboy.tick(10)
+                pyboy.button ('a')
+                data = [
+                    str(fightnum),
+                    'bulbasaur',
+                    movedict[pyboy.memory[0xCCDC]],
+                    str(False if (crit_flag == 0) else True),
+                    str(True if (miss_flag == 0) else False),
+                    str(pyboy.memory[mem_values.pkmn1_hp + 1]),
+                    str(pyboy.memory[0xCFE7])
+                ]
+                if crit_flag == 1:
+                    crit_flag = 0
+                if miss_flag == 1:
+                    miss_flag = 0
+                # writer.writerow(data)
+                
         
-    if counter == 50:
-        # print(str(wins) + " " + str (losses))
-        counter = 0
-    
-    if pyboy.memory[mem_values.pkmn1_hp + 1] == 0:
-        losses += 1
-        counter = -1
-        fightnum += 1
-        with open("pokemonai\\state_file_6.state", "rb") as f:
-            pyboy.load_state(f)
-    if (pyboy.memory[mem_values.e_pkmn1_hp + 1] == 0) and (pyboy.memory[mem_values.e_pkmn2_hp + 1] == 0):
-        wins += 1
-        counter = -1
-        fightnum += 1
-        with open("pokemonai\\state_file_6.state", "rb") as f:
-            pyboy.load_state(f)
+        counter += 1
+        if counter % 5 == 0:
+            if pyboy.memory[0xCC26] == 0 and pyboy.memory[0xCC25] == 9:
+                if (pyboy.memory[0xCCDD] != 0):
+                    data = [
+                    str(fightnum),
+                    pokedex[pyboy.memory[0xCFD8]],
+                    movedict[pyboy.memory[0xCCDD]],
+                    str(False if (crit_flag == 0) else True),
+                    str(True if (miss_flag == 0) else False),
+                    str(pyboy.memory[mem_values.pkmn1_hp + 1]),
+                    str(pyboy.memory[0xCFE7])
+                ]
+                if crit_flag == 1:
+                    crit_flag = 0
+                if miss_flag == 1:
+                    miss_flag = 0
+                # writer.writerow(data)
+                pyboy.button ('a')
+                pyboy.tick(10)
+                if (pyboy.memory[0xD02F] == 0):
+                    attack = rand.randint(1, 2)
+                else :
+                    attack = rand.randint(1, 3)
+                while pyboy.memory[0xCC26] != attack:
+                    pyboy.button ('down')
+                    pyboy.tick(10)
+                pyboy.button ('a')
+                turns += 1
+                data = [
+                    str(fightnum),
+                    'bulbasaur',
+                    movedict[pyboy.memory[0xCCDC]],
+                    str(False if (crit_flag == 0) else True),
+                    str(True if (miss_flag == 0) else False),
+                    str(pyboy.memory[mem_values.pkmn1_hp + 1]),
+                    str(pyboy.memory[0xCFE7])
+                ]
+                if crit_flag == 1:
+                    crit_flag = 0
+                if miss_flag == 1:
+                    miss_flag = 0
+                # writer.writerow(data)
+            elif (pyboy.memory[0xCC26] >=0 and pyboy.memory[0xCC26] <= 2) and pyboy.memory[0xCC25] == 5:
+                pyboy.button ('a')
+                pyboy.tick(10)
+            else:
+                if pyboy.memory[0xCC26] == 1:
+                    pyboy.button ('up')
+                if pyboy.memory[0xCC25] == 15:
+                    pyboy.button ('left')
+            
+        if counter == 50:
+            # print(str(wins) + " " + str (losses))
+            counter = 0
         
-    
+        if pyboy.memory[mem_values.pkmn1_hp + 1] == 0:
+            writer.writerow(['False', str(turns)])
+            losses += 1
+            counter = -1
+            fightnum += 1
+            turns = 0
+            with open("pokemonai\\state_file_6.state", "rb") as f:
+                pyboy.load_state(f)
+        if (pyboy.memory[mem_values.e_pkmn1_hp + 1] == 0) and (pyboy.memory[mem_values.e_pkmn2_hp + 1] == 0):
+            writer.writerow(['True', str(turns)])
+            wins += 1
+            counter = -1
+            fightnum += 1
+            turns = 0
+            with open("pokemonai\\state_file_6.state", "rb") as f:
+                pyboy.load_state(f)
+        pass
 
 pyboy.stop()
