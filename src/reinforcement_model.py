3c8f00034051857a68770c3c4459df610bce76d9
Daniel Gu <bobthebuilder10492@gmail.com>
2024-07-16 08:16:01 +0800

    misc: add notes for RL implementation

---
+++ src/reinforcement_model.py
@@ -0,0 +1,4 @@
+# Notes for implementation
+# use `collections.defaultdict` for the state dict.
+# `typing.NamedTuple` can encode names for each thing in the state without significant overhead.
+# it might be worth doing the same for the output space (lists inside tuples are still mutable)?
